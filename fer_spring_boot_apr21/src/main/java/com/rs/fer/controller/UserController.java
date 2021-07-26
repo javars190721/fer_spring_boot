a65fcfa0cefd130279076e82f4650e5e9bc36970
Satish <Satish@Sriramaneni>
2021-07-26 11:47:37 +0530

    Removed unnecessary methods

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -54,56 +55,6 @@ public class UserController {
 		}
 		return response;
 	}
-	/*
-	@PostMapping("/login")
-	public LoginResponse login(@RequestBody LoginRequest request) {
-
-		LoginResponse response = null;
-
-		Set<String> errorMessages = userValidation.validateLoginRequest(request);
-		// return response with error messages
-		if (!CollectionUtils.isEmpty(errorMessages)) {
-			response = new LoginResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
-
-		} else {
-			response = userService.login(request);
-		}
-		return response;
-	}
-	*/
-	// get User
-
-	@GetMapping("/user/{id}")
-	public GetUserResponse getUser(@PathVariable("id") Integer id) {
-		GetUserResponse response = null;
-		Set<String> errorMessages = userValidation.validateGetUserRequest(id);
-		if (!CollectionUtils.isEmpty(errorMessages)) {
-			response = new GetUserResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
-		} else {
-			response = userService.getUser(id);
-		}
-		return response;
-	}
 	
-	@PostMapping("/updateUser")
-
-	public UpdateUserResponse updateuser(@RequestBody UpdateUserRequest request) {
-
-		UpdateUserResponse response = null;
-
-		Set<String> errorMessages = userValidation.validateUpdateUserRequest(request);
-
-		if (!CollectionUtils.isEmpty(errorMessages)) {
-			// return response with error messages
-			response = new UpdateUserResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
-
-		} else {
-			response = userService.updateuser(request);
-		}
-
-		return response;
-
-	}
-
-
+	
 }
