58098eedee569e2bb9bcad47c0edf3b837fe1c24
intur <intur@AshokKumar>
2021-07-26 17:48:33 +0530

    i am don with updateuser

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rs.fer.user.request.RegistrationRequest;
+import com.rs.fer.user.request.UpdateUserRequest;
 import com.rs.fer.user.response.RegistrationResponse;
+import com.rs.fer.user.response.UpdateUserResponse;
 import com.rs.fer.user.service.UserService;
 import com.rs.fer.user.validation.UserValidation;
 
@@ -47,6 +49,26 @@ public class UserController {
 		}
 		return response;
 	}
+	@PostMapping("/updateUser")
+
+	public UpdateUserResponse updateuser(@RequestBody UpdateUserRequest request) {
+
+		UpdateUserResponse response = null;
+
+		Set<String> errorMessages = userValidation.validateUpdateUserRequest(request);
+
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			// return response with error messages
+			response = new UpdateUserResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = userService.updateuser(request);
+		}
+
+		return response;
+
+	}
+
 	
 	
 }
