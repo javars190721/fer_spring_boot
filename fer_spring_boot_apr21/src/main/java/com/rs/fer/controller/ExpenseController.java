87bd6230fb6b843ed2b666e9476e76e9061f128d
goutham <javars.19072021@gmail.com>
2021-07-27 20:47:13 +0530

    -commit add expense (pending-not working for success point of view)

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rs.fer.expense.request.DeleteExpenseRequest;
+import com.rs.fer.expense.request.EditExpenseRequest;
 import com.rs.fer.expense.response.DeleteExpenseResponse;
+import com.rs.fer.expense.response.EditExpenseResponse;
 import com.rs.fer.expense.service.ExpenseService;
 import com.rs.fer.expense.validation.ExpenseValidation;
 
@@ -24,6 +27,27 @@ public class ExpenseController {
 
 	@Autowired
 	ExpenseService expenseService;
+	
+	@PostMapping("/editExpense")
+	public EditExpenseResponse editExpense(@RequestBody EditExpenseRequest request) {
+
+		EditExpenseResponse response = null;
+
+		Set<String> errorMessages = expenseValidation.validateEditExpenseRequest(request);
+
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+
+			// Return response with error messages
+
+			response = new EditExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = expenseService.editExpense(request);
+		}
+
+		return response;
+
+	}
 	
 	@DeleteMapping("/deleteExpense")
 
