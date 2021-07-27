e7883b40da316672106e1b5806e9af6b7f29bf6d
intur <lakshmi>
2021-07-27 05:55:38 +0530

    delete Expense don sir

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/ExpenseController.java
@@ -1,9 +1,17 @@
 package com.rs.fer.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rs.fer.expense.request.DeleteExpenseRequest;
+import com.rs.fer.expense.response.DeleteExpenseResponse;
 import com.rs.fer.expense.service.ExpenseService;
 import com.rs.fer.expense.validation.ExpenseValidation;
 
@@ -16,6 +24,29 @@ public class ExpenseController {
 
 	@Autowired
 	ExpenseService expenseService;
+	
+	@DeleteMapping("/deleteExpense")
+
+	public DeleteExpenseResponse deleteExpense(@RequestBody DeleteExpenseRequest request) {
+
+		DeleteExpenseResponse response = null;
+
+		Set<String> errorMessages = expenseValidation.validateDeleteExpenseRequest(request);
+
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			
+			
+			// Return response with error messages
+
+			response = new DeleteExpenseResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = expenseService.deleteExpense(request);
+		}
+
+		return response;
+
+	}
 	
 
 	
